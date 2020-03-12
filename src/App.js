7843db71001ed4966ee7c2ce96ab135695ff9177
MariSandvik <maricelina@live.com>
2020-03-12 11:34:35 +0100

    Created a Content.js file

--- src/App.js
+++ src/App.js
@@ -1,28 +1,20 @@
 import React from 'react';
 import logo from './logo.svg';
 import Heading from "./components/layout/Heading";
+import Content from "./components/layout/Content";
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <Heading title="Title from prop" subtitle="Subtitle from prop" />
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+        <Heading title="Title from prop" subtitle="Subtitle from prop" />
+        
+        
+        <Content>
+        This will be the children prop inside the component.
+        </Content>
+        </div>
+    );
 }
 
 export default App;
